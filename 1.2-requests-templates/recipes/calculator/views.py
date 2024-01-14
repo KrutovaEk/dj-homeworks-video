d27586152aab55b01168a8d1b8ca606da319ba2b
KrutuvaEkaterin <krutova3113@mail.ru>
2024-01-14 14:54:22 +0500

    Task_1.2

---
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.template.context_processors import request
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def index(request, recipes):
+    servings = int(request.GET.get("servings", 1))
+    t = DATA.get(recipes)
+    f = {}
+    for ingredient, amount in t.items():
+        amounts = float(amount) * servings
+        f[ingredient] = amounts
+    context = {}
+    context['recipe'] =f
+    return render(request, 'calculator/index.html', context)
+
